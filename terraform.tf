043c038935fa00dccd899077a4e9b04e73c91cb1
e <your.email@example.com>
2026-02-06 14:53:23 +0000

    first commit

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.58.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  features {}
+}
+
+
+resource "azurerm_resource_group" "connection_test" {
+  name     = "terraform-test-rg"
+  location = "north europe"
+}
